4f3054b46b444726dbae7845430f76f0eb1ab6bd
wend69 <sukhbatgansukh09@gmail.com>
2026-04-01 22:49:30 +0800

    mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -3,13 +3,20 @@
 # Exercise 1.7
 # mortgage.py
 
-principal = 500000.0
+principal = 500000
+month = 0
 rate = 0.05
 payment = 2684.11
-total_paid = 0.0
+extra = 1000
+total_paid = 0
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
-
-print('Total paid', round(total_paid, 6))
+    month += 1
+    if month <= 12:
+       the_payment = payment + 1000
+    else:
+       the_payment = payment
+    principal = principal * (1+rate/12) - the_payment
+    total_paid = total_paid + the_payment
+print('Total paid', round(total_paid, 1))
+print('Months', month)
--- Work/test2.py
+++ Work/test2.py
@@ -1,1 +1,19 @@
 
+principal = 500000
+month = 0
+rate = 0.05
+payment = 2684.11
+extra = 1000
+total_paid = 0
+
+while principal > 0:
+    month += 1
+    if month <= 12:
+       the_payment = payment + 1000
+    else:
+       the_payment = payment
+    principal = principal * (1+rate/12) - the_payment
+    total_paid = total_paid + the_payment
+print('Total paid', round(total_paid, 1))
+print('Months', month)
+
